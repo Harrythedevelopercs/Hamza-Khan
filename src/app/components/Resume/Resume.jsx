78b22cf53d0d47bce12f1bd8bde8fe338b6be535
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-10-04 03:16:10 +0500

    new change

--- src/app/components/Resume/Resume.jsx
+++ src/app/components/Resume/Resume.jsx
@@ -5,7 +5,7 @@ import Style from './resume.module.css'
 export default function Resume() {
     return (
         <div className={Style.container}>
-            <h2 className={`${Style.Column} group-hover:text-[40px] group-hover:font-extralight`}>Resume </h2>
+            <h2 className={`${Style.Column} xs:group-hover:text-[15px] md:group-hover:text-[40px] group-hover:font-extralight`}>Resume </h2>
             <Image src={Arrow} alt="Hamza Khan" className={`${Style.ReadMoreImage} group-hover:opacity-100 opacity-0 group-hover:rotate-90 `} />
         </div>
     )
